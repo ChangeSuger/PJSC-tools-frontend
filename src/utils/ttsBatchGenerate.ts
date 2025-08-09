6403246b618999b04f024a6de9a91eeca61f30b4
ChangeSuger <camille_0315@buaa.edu.cn>
2025-08-09 13:50:23 +0800

    refactor: 音频生成方法提取为公共函数

---
+++ src/utils/ttsBatchGenerate.ts
@@ -0,0 +1,119 @@
+import { useAudioDBStore, useSettingsStore, useTTSCharacterStore, useTTSModelStore } from "@/stores";
+import type { ExampleAudioObject, StoryItem, TTSGenerateSSEData } from "@/types";
+import { DEFAULT_EMOTION_CLASS } from '@/datas';
+import { Message } from "@arco-design/web-vue";
+import { CommonApi } from "@/api";
+
+/**
+ * 参考音频兜底策略：情感标签的参考音频 > 情感分类的参考音频 > 默认的参考音频
+ * @param cid
+ * @param emotion
+ */
+export async function getExampleAudio(
+  cid: string,
+  emotion: string,
+): Promise<ExampleAudioObject | null> {
+  const exampleAudioDB = useAudioDBStore().getExampleAudioDB;
+  const emotionMap = useTTSCharacterStore().getEmotionMap;
+
+  let exampleAudio = await exampleAudioDB.getExampleAudio(`${cid}-${emotion}`);
+
+  if (!exampleAudio) {
+    const emotionClass = emotionMap[emotion];
+
+    if (emotionClass) {
+      exampleAudio = await exampleAudioDB.getExampleAudio(`${cid}-${emotionClass}`);
+    }
+
+    if (exampleAudio && emotionClass !== '中立') {
+      exampleAudio = await exampleAudioDB.getExampleAudio(`${cid}-${DEFAULT_EMOTION_CLASS}`);
+    }
+  }
+
+  return exampleAudio;
+}
+
+
+export async function ttsBatchGenerateJP(
+  storyItem: StoryItem,
+  audioId: string,
+  onSSEProcess: (data: TTSGenerateSSEData) => void,
+) {
+  const { cid, lineJP, emotion } = storyItem;
+
+  const settingsStore = useSettingsStore();
+  const ttsCharacterStore = useTTSCharacterStore();
+
+
+  const ttsConfigDefault = settingsStore.getTTSConfig;
+  const ossConfig = settingsStore.getOSSConfig;
+
+  const ttsConfig = {
+    ...ttsConfigDefault,
+    ...ttsCharacterStore.ttsCharacterConfigMap[cid]
+  };
+
+  const modelConfig = ttsCharacterStore.getModelByCharacterAndEmotion(cid, emotion);
+
+  if (!modelConfig) {
+    Message.error('未找到该角色的模型配置，请在配置角色模型后重试。');
+    return;
+  }
+
+  const { sovitsModel, gptModel } = modelConfig!;
+
+  const changeModelResult = await useTTSModelStore().changeModel(sovitsModel, gptModel);
+
+  if (!changeModelResult) {
+    Message.error('切换模型失败，音频生成任务停止。');
+    return;
+  }
+
+  const exampleAudio = await getExampleAudio(cid, emotion);
+
+  if (!exampleAudio) {
+    Message.error('未找到合适的参考音频，请在设置参考音频后重试。');
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append('ttsConfig', JSON.stringify(ttsConfig));
+  formData.append('ossConfig', JSON.stringify(ossConfig));
+  formData.append('exampleAudio', exampleAudio!.audio);
+  formData.append('exampleText', JSON.stringify({
+    text: exampleAudio!.text,
+    lang: '日文',
+  }));
+  formData.append('targetText', JSON.stringify({
+    text: lineJP,
+    id: audioId,
+    lang: '日文',
+  }));
+
+  const res = await CommonApi.ttsBatchGenerate(formData);
+
+  if (res.code === 200) {
+    const eventSource = new EventSource('/api/tts-sse');
+
+    await new Promise((res, rej) => {
+      eventSource.onmessage = (event) => {
+        const progressData: TTSGenerateSSEData = JSON.parse(event.data);
+
+        if (progressData.code === 2) {
+          console.log('TTS End');
+          eventSource.close();
+          res('');
+        } else if (progressData.code === 0) {
+          console.log('TTS Start')
+        } else if (progressData.code === 1) {
+          onSSEProcess(progressData);
+        }
+      };
+
+      eventSource.onerror = (error) => {
+        console.error('SSE Failed', error);
+        rej();
+      }
+    });
+  }
+}
--- src/views/TTSView.vue
+++ src/views/TTSView.vue
@@ -58,26 +58,22 @@
 </template>
 
 <script setup lang="ts">
-import { CommonApi } from '@/api';
-import { useSettingsStore, useAudioDBStore, useTTSCharacterStore, useTTSModelStore } from '@/stores';
+import { useSettingsStore, useTTSCharacterStore } from '@/stores';
 import { computed, ref } from 'vue';
 
 import StoryViewerItem from '@/components/story/StoryViewerItem.vue';
 import ExampleAudioConfigDialog from '@/components/tts/exampleAudio/ExampleAudioConfigDialog.vue';
 import TTSCharacterConfigDialog from '@/components/tts/TTSCharacterConfigDialog.vue';
 import CharacterModelConfigDialog from '@/components/tts/CharacterModelConfigDialog.vue';
-import { Message } from '@arco-design/web-vue';
-import type { ExampleAudioObject, StoryItem, StoryScriptFull, TTSGenerateSSEData } from '@/types';
+import type { StoryItem, StoryScriptFull, TTSGenerateSSEData } from '@/types';
 import { scriptAdaptIn } from '@/utils/scriptAdapter';
 import ChangeModelForm from '@/components/tts/ChangeModelForm.vue';
 import ManageScriptJSON from '@/components/common/ManageScriptJSON.vue';
 import ManageEmotionDialog from '@/components/tts/manageEmition/ManageEmotionDialog.vue';
-import { DEFAULT_EMOTION_CLASS } from '@/datas';
+import { ttsBatchGenerateJP as ttsBatchGenerate } from '@/utils/ttsBatchGenerate';
 
 const settingsStore = useSettingsStore();
-const audioDBStore = useAudioDBStore();
 const ttsCharacterStore = useTTSCharacterStore();
-const ttsModelStore = useTTSModelStore();
 
 const manageScriptJSONRef = ref<InstanceType<typeof ManageScriptJSON>>();
 
@@ -107,114 +103,21 @@ const storyListFilter = computed(() => {
   }
 });
 
-/**
- * 参考音频兜底策略：情感标签的参考音频 > 情感分类的参考音频 > 默认的参考音频
- * @param cid
- * @param emotion
- */
-async function getExampleAudio(
-  cid: string,
-  emotion: string,
-): Promise<ExampleAudioObject | null> {
-  const exampleAudioDB = audioDBStore.getExampleAudioDB;
-  const emotionMap = ttsCharacterStore.getEmotionMap;
-
-  let exampleAudio = await exampleAudioDB.getExampleAudio(`${cid}-${emotion}`);
-
-  if (!exampleAudio) {
-    const emotionClass = emotionMap[emotion];
-
-    if (emotionClass) {
-      exampleAudio = await exampleAudioDB.getExampleAudio(`${cid}-${emotionClass}`);
-    }
-
-    if (exampleAudio && emotionClass !== '中立') {
-      exampleAudio = await exampleAudioDB.getExampleAudio(`${cid}-${DEFAULT_EMOTION_CLASS}`);
-    }
-  }
-
-  return exampleAudio;
-}
-
 async function ttsBatchGenerateJP(storyItem: StoryItem) {
-  const { id, cid, lineJP, emotion } = storyItem;
-
-  const ttsConfigDefault = settingsStore.getTTSConfig;
-  const ossConfig = settingsStore.getOSSConfig;
-
-  const ttsConfig = {
-    ...ttsConfigDefault,
-    ...ttsCharacterStore.ttsCharacterConfigMap[cid]
-  };
-
-  const modelConfig = ttsCharacterStore.getModelByCharacterAndEmotion(cid, emotion);
-
-  if (!modelConfig) {
-    Message.error('未找到该角色的模型配置，请在配置角色模型后重试。');
-    return;
-  }
-
-  const { sovitsModel, gptModel } = modelConfig!;
-
-  const changeModelResult = await ttsModelStore.changeModel(sovitsModel, gptModel);
-
-  if (!changeModelResult) {
-    Message.error('切换模型失败，音频生成任务停止。');
-    return;
-  }
-
-  const exampleAudio = await getExampleAudio(cid, emotion);
-
-  if (!exampleAudio) {
-    Message.error('未找到合适的参考音频，请在设置参考音频后重试。');
-    return;
-  }
-
-  const scriptName = manageScriptJSONRef.value!.getScriptName();
-
-  const formData = new FormData();
-  formData.append('ttsConfig', JSON.stringify(ttsConfig));
-  formData.append('ossConfig', JSON.stringify(ossConfig));
-  formData.append('exampleAudio', exampleAudio!.audio);
-  formData.append('exampleText', JSON.stringify({
-    text: exampleAudio!.text,
-    lang: '日文',
-  }));
-  formData.append('targetText', JSON.stringify({
-    text: lineJP,
-    id: generateAudioID(id, scriptName, 'jp'),
-    lang: '日文',
-  }));
-
-  const res = await CommonApi.ttsBatchGenerate(formData);
-
-  if (res.code === 200) {
-    const eventSource = new EventSource('/api/tts-sse');
-
-    await new Promise((res, rej) => {
-      eventSource.onmessage = (event) => {
-        const progressData: TTSGenerateSSEData = JSON.parse(event.data);
-
-        if (progressData.code === 2) {
-          console.log('TTS End');
-          eventSource.close();
-          res('');
-        } else if (progressData.code === 0) {
-          console.log('TTS Start')
-        } else if (progressData.code === 1) {
-          const url = progressData.url;
-          if (!storyItem.jpAudioURLs.includes(url)) {
-            storyItem.jpAudioURLs.push(url);
-          }
-        }
-      };
-
-      eventSource.onerror = (error) => {
-        console.error('SSE Failed', error);
-        rej();
+  await ttsBatchGenerate(
+    storyItem,
+    generateAudioID(
+      storyItem.id,
+      manageScriptJSONRef.value!.getScriptName(),
+      'jp',
+    ),
+    (progressData: TTSGenerateSSEData) => {
+      const url = progressData.url;
+      if (!storyItem.jpAudioURLs.includes(url)) {
+        storyItem.jpAudioURLs.push(url);
       }
-    });
-  }
+    }
+  );
 }
 
 async function ttsGenerateAllJP() {
